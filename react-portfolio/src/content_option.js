1fcddbf2577bc290dd030c109b9524d5466013f3
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-06-12 17:50:53 +0545

    initial commit

---
+++ react-portfolio/src/content_option.js
@@ -0,0 +1,100 @@
+const logotext = "Prasanna";
+const meta = {
+    title: "Prasanna Shrestha",
+    description: "",
+};
+
+const introdata = {
+    title: "I’m Prasanna Shrestha",
+    animated: {
+        first: "coder",
+        second: "I code web applications",
+        third: "I develop mobile apps",
+    },
+    
+    your_img_url: "https://scontent.fktm10-1.fna.fbcdn.net/v/t39.30808-6/337396412_1186460145342929_521181232617087978_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=XXSkls5S_CYQ7kNvgEcan5Y&_nc_ht=scontent.fktm10-1.fna&oh=00_AYD77LAjq-LIH1IdpyGHwfvWn4yd0U5kY_a8MBegzSrUew&oe=666F569A",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [
+];
+
+const skills = [{
+        name: "Go lang",
+        img:'https://djeqr6to3dedg.cloudfront.net/repo-logos/library/golang/live/logo.png',
+        value: 90,
+    },
+    
+    {
+        name: "Type-script",
+        img:'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+        value: 90,
+    },
+    {
+        name: "React",
+        img:'https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=webp&w=256',
+        value: 90,
+    },
+    {
+        name:"Mysql",
+        img:'https://dh.library.virginia.edu/system/files/styles/large/private/MySQL.PNG?itok=y4tI90y8',
+        value:90,
+    }
+    
+];
+
+const services = [
+    {
+        title: "Web Apps",
+        description: "Create efficient web applications using GoLang for the backend and TypeScript with React for the frontend.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Develop hybrid mobile applications with React Native.",
+    },
+];
+
+const dataportfolio = [
+   
+   
+
+   
+ 
+  
+   
+   
+  
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shresthaprashanna27@gmail.com",
+    YOUR_FONE: "9765546542",
+    description: " I am a coding enthusiast. I love to code applications with a frontend in React and a backend in Go.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/prasannashrestha011",
+    facebook: "https://www.facebook.com/profile.php?id=100088972596518",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
